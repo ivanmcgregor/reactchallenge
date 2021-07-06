2e8f7342c48a5d49970b62b7e913dae4b716b78b
Ivan McGregor <ivanmcgregor@web.de>
2021-07-06 11:47:12 +0200

    chore: setup typescript
    to get static code analysis during development

---
+++ src/components/PlayerCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PlayerCard() {
+  return (
+    <div className= "App" >
+    <p>
+    This will be a player card
+      </p>
+      </div>
+  );
+}
+
+export default PlayerCard;
